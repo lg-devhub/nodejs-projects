25e897b2b25fe26c606c5ef420880a2236baebd5
lg-devhub <ogusta321@gmail.com>
2026-08-10 17:20:14 -0300

    typescript updated | added tsx transpile | tsup proccessing

---
+++ typescript-node-projects/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(async function startProgram() {
+    console.log("Hello World!");
+})();
+async function letEmail() {
+    let myEmail = "luiz@gmail.com";
+    console.log(myEmail);
+}
+letEmail();
+//# sourceMappingURL=index.js.map
---
+++ typescript-node-projects/src/index.ts
@@ -0,0 +1,10 @@
+(async function startProgram() {
+    console.log("Hello World!");
+})();
+
+async function letEmail() {
+    let myEmail: string = "luiz@gmail.com";
+    console.log(myEmail);
+}
+
+letEmail();
